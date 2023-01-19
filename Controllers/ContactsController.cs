22c132b367b2af5f586472bec4e029aa62d1dba9
Pavle9699 <pavleiq99@gmail.com>
2023-01-19 22:38:00 +0100

    Add project files.

---
+++ Controllers/ContactsController.cs
@@ -0,0 +1,23 @@
+﻿using ContactsAPI.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContactsAPI.Controllers
+{
+    [ApiController] //prvo anotacija da je api controller a ne mvc cntrlr
+    [Route("api/[controller]")]//ubacuje ime ovog kontrolera unutra
+    public class ContactsController : Controller
+    {
+        private readonly ContactsAPIDbContext dbContext;
+
+        public ContactsController(ContactsAPIDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]//za swagger
+        public IActionResult GetContacts()
+        {
+            return View();
+        }
+    }
+}
